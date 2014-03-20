1654b608985534db85758bc3a7559ad109773aae
Badger DuBois <lyreoforpheus@gmail.com>
2014-03-20 06:01:11 -0400

    Initial agnu

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AgnuApp::Application.config.secret_token = '2d186507230623e228c265ad6f8b74f60f6b8582d291198eb6df80084ed0e8162556522496cc88958207a4c0a38b2b5e680d422665d53b615f33c84ba64bc3e0'
