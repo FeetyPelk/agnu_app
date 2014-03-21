49bc5067788a9852c32c87fa12fe3cb75e16c196
Badger DuBois <lyreoforpheus@gmail.com>
2014-03-21 08:42:31 -0400

    Bailing out on this one

--- config/application.rb
+++ config/application.rb
@@ -1,11 +1,14 @@
 require File.expand_path('../boot', __FILE__)
 
 # Pick the frameworks you want:
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "active_resource/railtie"
-require "sprockets/railtie"
+require 'rails/all'
+ # Following commented out 3/21/2014
+#require "active_record/railtie"
+#require "action_controller/railtie"
+#require "action_mailer/railtie"
+#require "active_resource/railtie"
+#require "sprockets/railtie"
+
 # require "rails/test_unit/railtie"
 
 if defined?(Bundler)
